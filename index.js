eda897825db17fcb233fd6d40460eebef53773af
Kalidas Rajeev <kalidasrajeev1997@gmail.com>
2023-09-19 09:53:46 +0530

    final

--- index.js
+++ index.js
@@ -6,42 +6,42 @@ console.log("---All---");
 console.log(all);
 
 // list all the food items with category vegetables
-const vegetables = food.filter((v) => v.category.includes("Vegetable"));
+const vegetables = food.filter((f) => f.category.includes("Vegetable"));
 console.log("---Vegetables---");
 console.log(vegetables);
 
 // list all the food items with category fruit
-const fruits = food.filter((v) => v.category.includes("Fruit"));
+const fruits = food.filter((f) => f.category.includes("Fruit"));
 console.log("---Fruits---");
 console.log(fruits);
 
 // list all the food items with category protien
-const protien = food.filter((v) => v.category.includes("Protein"));
+const protien = food.filter((f) => f.category.includes("Protein"));
 console.log("---Protein---");
 console.log(protien);
 
 // list all the food items with category nuts
-const nuts = food.filter((v) => v.category.includes("Nuts"));
+const nuts = food.filter((f) => f.category.includes("Nuts"));
 console.log("---Nuts---");
 console.log(nuts);
 
 // list all the food items with category grains
-const grains = food.filter((v) => v.category.includes("Grain"));
+const grains = food.filter((f) => f.category.includes("Grain"));
 console.log("---Grains---");
 console.log(grains);
 
 // list all the food items with category dairy
-const dairy = food.filter((v) => v.category.includes("Dairy"));
+const dairy = food.filter((f) => f.category.includes("Dairy"));
 console.log("---Dairy---");
 console.log(dairy);
 
 // list all the food items with calorie above 100
-const calorieAbove100 = food.filter((v) => v.calorie > 100);
+const calorieAbove100 = food.filter((f) => f.calorie > 100);
 console.log("---Calorie Above 100---");
 console.log(calorieAbove100);
 
 // list all the food items with calorie below 100
-const calorieBelow100 = food.filter((v) => v.calorie < 100);
+const calorieBelow100 = food.filter((f) => f.calorie < 100);
 console.log("---Calorie Below 100---");
 console.log(calorieBelow100);
 
