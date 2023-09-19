e971e379a03b821d05b40b33b7e7cef6cfc8e61c
Kalidas Rajeev <kalidasrajeev1997@gmail.com>
2023-09-19 09:49:04 +0530

    function remove

--- index.js
+++ index.js
@@ -1,52 +1,47 @@
 const food = require("./food.json");
 
-const handleFilter = (condition) => {
-  const filteredArray = food.filter(condition);
-  return filteredArray;
-};
-
 // list all the food items
-const all = handleFilter(() => true);
+const all = food;
 console.log("---All---");
 console.log(all);
 
 // list all the food items with category vegetables
-const vegetables = handleFilter((v) => v.category.includes("Vegetable"));
+const vegetables = food.filter((v) => v.category.includes("Vegetable"));
 console.log("---Vegetables---");
 console.log(vegetables);
 
 // list all the food items with category fruit
-const fruits = handleFilter((v) => v.category.includes("Fruit"));
+const fruits = food.filter((v) => v.category.includes("Fruit"));
 console.log("---Fruits---");
 console.log(fruits);
 
 // list all the food items with category protien
-const protien = handleFilter((v) => v.category.includes("Protein"));
+const protien = food.filter((v) => v.category.includes("Protein"));
 console.log("---Protein---");
 console.log(protien);
 
 // list all the food items with category nuts
-const nuts = handleFilter((v) => v.category.includes("Nuts"));
+const nuts = food.filter((v) => v.category.includes("Nuts"));
 console.log("---Nuts---");
 console.log(nuts);
 
 // list all the food items with category grains
-const grains = handleFilter((v) => v.category.includes("Grain"));
+const grains = food.filter((v) => v.category.includes("Grain"));
 console.log("---Grains---");
 console.log(grains);
 
 // list all the food items with category dairy
-const dairy = handleFilter((v) => v.category.includes("Dairy"));
+const dairy = food.filter((v) => v.category.includes("Dairy"));
 console.log("---Dairy---");
 console.log(dairy);
 
 // list all the food items with calorie above 100
-const calorieAbove100 = handleFilter((v) => v.calorie > 100);
+const calorieAbove100 = food.filter((v) => v.calorie > 100);
 console.log("---Calorie Above 100---");
 console.log(calorieAbove100);
 
 // list all the food items with calorie below 100
-const calorieBelow100 = handleFilter((v) => v.calorie < 100);
+const calorieBelow100 = food.filter((v) => v.calorie < 100);
 console.log("---Calorie Below 100---");
 console.log(calorieBelow100);
 
